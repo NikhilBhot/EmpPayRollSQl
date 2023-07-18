c7776e6ce6abb6c3732c646075eaa38ab7dfa4d6
unknown <bhotnikhil25@gmail.com>
2023-07-18 11:37:05 +0530

    [Nikhil]-CreateDB ON SQL

---
+++ SQLQuery5.sql
@@ -0,0 +1,19 @@
+
+/*Ability to create a payroll
+service database
+- Using MSSQL Client use create database
+query to create a payroll_service
+database
+- Also you can see the DB created by
+using show database query
+- And go to the database created by using
+use payroll_service query*/
+
+
+CREATE DATABASE payroll_serviceRFP287;
+
+USE payroll_service;
+
+
+
+
